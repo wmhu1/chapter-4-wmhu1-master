1447d9c008275c4815f0cc0790e039214c00293c
wmhu1 <42479515+wmhu1@users.noreply.github.com>
2018-11-19 15:09:27 -0600

    working on student program, almost finished

--- Assignments/Student.java
+++ Assignments/Student.java
@@ -8,23 +8,75 @@ public class Student
 {
    private String firstName, lastName;
    private Address homeAddress, schoolAddress;
-
+   private int s1;
+   private int s2;
+   private int s3;
+   private int average;
    //-----------------------------------------------------------------
    //  Sets up this Student object with the specified initial values.
    //-----------------------------------------------------------------
    public Student (String first, String last, Address home,
-                   Address school)
+                   int score1,int score2,int score3)
    {
       firstName = first;
       lastName = last;
       homeAddress = home;
-      schoolAddress = school;
+      schoolAddress = new Address ("440 W. Aurora Ave", "Naperville",
+                                    "IL",60540);
+      s1 = score1;
+      s2 = score2;
+      s3 = score3;
    }
-   public Student setTestScore(testnum, score)
+   public Student (String first, String last, Address home)
    {
-       
+      firstName = first;
+      lastName = last;
+      homeAddress = home;
+      schoolAddress = new Address ("440 W. Aurora Ave", "Naperville",
+                                    "IL",60540);
+      s1 = 0;
+      s2 = 0;
+      s3 = 0;
+   }
+   public void setTestScore(int testnum, int score)
+   {
+       if (testnum == 1)
+       {
+           s1 = score;
+        }
+       if (testnum == 2)
+       {
+           s2 = score;
+        }
+       if (testnum == 3)
+       {
+           s3 = score;
+        }
     }
-
+   public int getTestScore(int testnum)
+   {
+       int testscore = 10;
+       if (testnum == 1)
+           testscore = s1;
+        
+       else if (testnum == 2)
+           testscore = s2;
+        
+       else if (testnum == 3)
+           testscore = s3;
+       else
+           System.out.println("You have entered an incorrect test number, your score has been set to 10");
+       return testscore;
+        
+    }
+   public int average(int score1,int score2, int score3)
+   {
+       s1 = score1;
+       s2 = score2;
+       s3 = score3;
+       average = (s1 + s2 + s3) / 3;
+       return average;
+    }   
    //-----------------------------------------------------------------
    //  Returns this Student object as a string.
    //-----------------------------------------------------------------
@@ -34,8 +86,10 @@ public class Student
 
       result = firstName + " " + lastName + "\n";
       result += "Home Address:\n" + homeAddress + "\n";
-      result += "School Address:\n" + schoolAddress;
-
+      result += "School Address:\n" + schoolAddress + "\n";
+      result += "Home Address:\n" + homeAddress + "\n";
+      result += "Test 1:" + s1 + "\tTest 2:" + s2 + "\tTest 3:"+ s3;
+      result += "\tTest Average:" + average;
       return result;
    }
 }
--- Assignments/StudentBody.java
+++ Assignments/StudentBody.java
@@ -10,8 +10,8 @@ public class StudentBody
    //-----------------------------------------------------------------
    public static void main (String[] args)
    {
-      Address school = new Address ("800 Lancaster Ave.", "Villanova",
-                                    "PA", 19085);
+      Address school = new Address ("440 W. Aurora Ave", "Naperville",
+                                    "IL", 19085);
 
       Address jHome = new Address ("21 Jump Street", "Lynchburg",
                                    "VA", 24551);
